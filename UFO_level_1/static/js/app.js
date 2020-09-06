49e7ef3ca2c19a80453b6a61ff6c32f4c42a0a00
Alexandra Taft <taft.alexandra@gmail.com>
2020-09-06 16:05:32 -0400

    html index file, updates to js code, work on filtering

---
+++ UFO_level_1/static/js/app.js
@@ -0,0 +1,40 @@
+// from data.js
+var tableData = data;
+
+// select the button
+var button = d3.select("#filter-btn");
+
+// select the form
+var form = d3.select(".form-group");
+
+// create event handlers
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+//complete event handler for form completion
+function runEnter() {
+    //prevent page from refreshing
+    d3.event.preventDefault();
+    //select input elemtent
+    var inputElement = d3.select(".form-control");
+    //get the value property
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    console.log(tableData);
+
+    var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
+    console.log(filteredData);
+
+    //print values to table
+    tableData.forEach(function(filteredData) {
+        var tbody = d3.select("tbody");
+        console.log(filteredData);
+        var row = tbody.append("tr");
+        Object.entries(filteredData).forEach(function([key, value]) {
+            console.log(key, value);
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+
+}
